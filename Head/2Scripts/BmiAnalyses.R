c755e59bf281793903b76f06298b12b0a2a36d3b
konstantinpopadin <konstantinpopadin@gmail.com>
2019-02-25 16:24:01 +0100

    better plots

--- Head/2Scripts/BmiAnalyses.R
+++ Head/2Scripts/BmiAnalyses.R
@@ -6,9 +6,14 @@ rm(list=ls(all=TRUE))
 Bmi = read.table("../../Body/1Raw/MainTable.txt", header = TRUE, sep = '\t')
 Bmi$Sex = Bmi$Sex-1; table(Bmi$Sex) # make it Dummy: 0 - males, 1 - females
 Bmi$BmiRatio = Bmi$BMI2/Bmi$BMI1
+Bmi$Bmi2Min1 = (Bmi$BMI2 - Bmi$BMI1)
 Bmi$mtDNARatio = Bmi$mtDNA2/Bmi$mtDNA1
+Bmi$mtDNA2Min1 = Bmi$mtDNA2 - Bmi$mtDNA1
 
-###### BMI decreased after operation (both types of operations, both genders)
+######## pdf
+pdf("../../Body/4Figures/Bmi.mtDNA.pdf")
+
+###### 1: BMI decreased after operation (both types of operations, both genders)
 summary(Bmi$BMI1)
 summary(Bmi$BMI2)
 wilcox.test(Bmi$BMI1,Bmi$BMI2, paired = TRUE)
@@ -35,21 +40,23 @@ a5<-lm(Bmi$BmiRatio ~ Bmi$mtDNARatio); summary(a5)
 a5<-lm(Bmi$BMI1 ~ Bmi$mtDNA1 + Bmi$Age); summary(a5)
 
 a6<-lm(Bmi$BMI2/Bmi$BMI1 ~ Bmi$BMI1); summary(a6) # decrease in BMI is more pronounced in case of high BMI1
+
 a7<-lm(Bmi$BMI2/Bmi$BMI1 ~ Bmi$BMI1 + Bmi$Age); summary(a7) # decrease in BMI is more pronounced in case of high BMI1 and less pronounced with Age
 a8<-lm(Bmi$BMI2/Bmi$BMI1 ~ Bmi$BMI1 + Bmi$Age + Bmi$mtDNA2 + Bmi$mtDNA1); summary(a8) # no effect of mtDNA
 a9<-lm(Bmi$BMI2/Bmi$BMI1 ~ Bmi$BMI1 + Bmi$Age + Bmi$mtDNA2/Bmi$mtDNA1); summary(a9) # no effect of mtDNA
 
+####### 2: mtDNA copies in controls and cases - before and after operations
 
-####### mtDNA increased after operation (both types of operations)
-summary(Bmi$mtDNA1)
-summary(Bmi$mtDNA2)
-wilcox.test(Bmi$mtDNA1,Bmi$mtDNA2, paired = TRUE)
-wilcox.test(Bmi[Bmi$Operation == 'LSG',]$mtDNA1,Bmi[Bmi$Operation == 'LSG',]$mtDNA2, paired = TRUE)  # 0.011
-wilcox.test(Bmi[Bmi$Operation == 'RYGB',]$mtDNA1,Bmi[Bmi$Operation == 'RYGB',]$mtDNA2, paired = TRUE)  # 0.0058
+boxplot(Bmi[Bmi$Operation == 'Controls' & Bmi$Sex == 0,]$mtDNA1,Bmi[Bmi$Operation == 'Controls' & Bmi$Sex == 1,]$mtDNA1, Bmi[Bmi$Operation != 'Controls' & Bmi$Sex == 0,]$mtDNA1,Bmi[Bmi$Operation != 'Controls' & Bmi$Sex == 1,]$mtDNA1, Bmi[Bmi$Operation != 'Controls' & Bmi$Sex == 0,]$mtDNA2,Bmi[Bmi$Operation != 'Controls' & Bmi$Sex == 1,]$mtDNA2, col = c('light blue', 'pink'))
+wilcox.test(Bmi[Bmi$Operation != 'Controls' & Bmi$Sex == 1,]$mtDNA1,Bmi[Bmi$Operation != 'Controls' & Bmi$Sex == 1,]$mtDNA2) # 4.829e-05
+wilcox.test(Bmi[Bmi$Operation == 'Controls' & Bmi$Sex == 1,]$mtDNA1,Bmi[Bmi$Operation != 'Controls' & Bmi$Sex == 1,]$mtDNA1) # 0.01726
+wilcox.test(Bmi[Bmi$Operation == 'Controls' & Bmi$Sex == 1,]$mtDNA1,Bmi[Bmi$Operation != 'Controls' & Bmi$Sex == 1,]$mtDNA2) # 0.6455
 
-######## pdf
 
-pdf("../../Body/4Figures/Bmi.mtDNA.pdf")
+####### 3: mtDNA increased after operation (both types of operations) for females but not males
+
+wilcox.test(Bmi[Bmi$Operation != 'Controls' & Bmi$Sex == 1,]$mtDNA1,Bmi[Bmi$Operation != 'Controls' & Bmi$Sex == 1,]$mtDNA2, paired = TRUE)  # 2.187e-05
+wilcox.test(Bmi[Bmi$Operation != 'Controls' & Bmi$Sex == 0,]$mtDNA1,Bmi[Bmi$Operation != 'Controls' & Bmi$Sex == 0,]$mtDNA2, paired = TRUE)  # 0.5566
 
 ### is there correlation between the strength of changes in BMI and strength of changes in mtDNA? mainly among females!!
 
@@ -74,23 +81,35 @@ for (i in 1:nrow(Bmi))
   { # i = 1
   if (!is.na(Bmi$mtDNA1[i]) & !is.na(Bmi$mtDNA2[i]) & !is.na(Bmi$BMI1[i]) & !is.na(Bmi$BMI2[i]))
     {
-    if (Bmi$mtDNARatio[i] >= 1) {segments(Bmi$mtDNA1[i], Bmi$BMI1[i], Bmi$mtDNA2[i], Bmi$BMI2[i], col = 'grey', lwd = 1) }
-    if  (Bmi$mtDNARatio[i] < 1) {segments(Bmi$mtDNA1[i], Bmi$BMI1[i], Bmi$mtDNA2[i], Bmi$BMI2[i], col = 'red', lwd = 1) }
+    #if (Bmi$mtDNARatio[i] >= 1) 
+    #  {
+      if (Bmi$Sex[i] == 1) {arrows(Bmi$mtDNA1[i], Bmi$BMI1[i], Bmi$mtDNA2[i], Bmi$BMI2[i], col = 'pink', lwd = 1)}
+      if (Bmi$Sex[i] == 0) {arrows(Bmi$mtDNA1[i], Bmi$BMI1[i], Bmi$mtDNA2[i], Bmi$BMI2[i], col = 'blue', lwd = 1)}
+      #segments(Bmi$mtDNA1[i], Bmi$BMI1[i], Bmi$mtDNA2[i], Bmi$BMI2[i], col = 'grey', lwd = 1)
+    #  }
+    # if  (Bmi$mtDNARatio[i] < 1) {segments(Bmi$mtDNA1[i], Bmi$BMI1[i], Bmi$mtDNA2[i], Bmi$BMI2[i], col = 'red', lwd = 1) }
     }
   if (Bmi$Operation[i] == 'Controls') 
     {
-    points(Bmi$mtDNA1[i],Bmi$BMI1[i], pch = 16, col = 'black')
+    if (Bmi$Sex[i] == 1) {points(Bmi$mtDNA1[i],Bmi$BMI1[i], pch = 16, col = 'pink')}
+    if (Bmi$Sex[i] == 0) {points(Bmi$mtDNA1[i],Bmi$BMI1[i], pch = 16, col = 'blue')}
     }
   }
 
 table(Bmi[!is.na(Bmi$mtDNARatio),]$Sex) # 10 32
 table(Bmi[Bmi$mtDNARatio < 1 & !is.na(Bmi$mtDNARatio),]$Sex) # 5 4 => amomng red more males. Out of 10, five behave "unnormally"
 
+cor.test(Bmi[Bmi$Sex == 1 & Bmi$Operation != 'Controls',]$BmiRatio,Bmi[Bmi$Sex == 1 & Bmi$Operation != 'Controls',]$mtDNARatio, method = 'spearman', alternative = 'less') # p = 0.04434, cor = 0.3578
+plot(Bmi[Bmi$Sex == 1 & Bmi$Operation != 'Controls',]$BmiRatio,Bmi[Bmi$Sex == 1 & Bmi$Operation != 'Controls',]$mtDNARatio) # p = 0.04434, cor = 0.3578
+
+plot(Bmi[Bmi$Sex == 1 & Bmi$Operation != 'Controls',]$Bmi2Min1,Bmi[Bmi$Sex == 1 & Bmi$Operation != 'Controls',]$mtDNA2Min1) # p = 0.04434, cor = 0.3578
+cor.test(Bmi[Bmi$Sex == 1 & Bmi$Operation != 'Controls',]$Bmi2Min1,Bmi[Bmi$Sex == 1 & Bmi$Operation != 'Controls',]$mtDNA2Min1, method = 'spearman')
+# dev.off()
+
 summary(Bmi[!is.na(Bmi$mtDNARatio) & Bmi$Sex == 0,]$mtDNARatio)
 summary(Bmi[!is.na(Bmi$mtDNARatio) & Bmi$Sex == 1,]$mtDNARatio)
 boxplot(Bmi[!is.na(Bmi$mtDNARatio) & Bmi$Sex == 0,]$mtDNARatio,Bmi[!is.na(Bmi$mtDNARatio) & Bmi$Sex == 1,]$mtDNARatio, notch = TRUE, names = c('males','females'), ylab = 'mtDNA2 / mtDNA1')
 abline(h = 1, col = 'red', lt = 2)
-
 
 # max in controls 
 MaxMtdnaControls = max(Bmi[Bmi$Operation == 'Controls',]$mtDNA1)
